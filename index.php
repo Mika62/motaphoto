43d366f3b05136bdf48e861f632500557d571b82
Mikael Fourré <mikael6287@live.fr>
2023-01-12 10:01:24 +0100

    Création de la section Hero

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php get_header() ?>
+<?php
+
+get_header();
+
+// Get all
+$posts = get_posts([
+    'posts_per_page' => 12,
+    'meta_key' => 'dates',
+    'orderby' => 'meta_value',
+    'post_type' => 'photos',
+]);
+
+// Get random
+$random = $posts[array_rand($posts)];
+?>
+
+<!-- Hero -->
+<section id="hero" class="hero">
+    <h1 class="hero__title">Photographe event</h1>
+    <img src="<?= get_esc_field('image.url', $random) ?>" alt="<?= get_esc_field('image.alt', $random) ?>" class="hero__img">
+</section>
 
 <?php get_footer() ?>
